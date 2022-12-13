02bf481ad5238a199f355a5cc1bea3a9277a02b8
taty2010 <shydalys123@gmail.com>
2022-12-13 14:36:12 -0600

    added serversideprops

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <div className="container">
       <Head>
@@ -16,7 +16,7 @@ export default function Home() {
         <Header title="Next.js Toolbox" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
+          {data.message} Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
@@ -25,4 +25,12 @@ export default function Home() {
       <Footer />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const data = {message: 'Hello World'}
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
